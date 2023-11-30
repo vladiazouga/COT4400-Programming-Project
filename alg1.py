7d67b80aab875a2586aa64f77742876211a8d5e1
vladiazouga <vzouga2021@fau.edu>
2023-11-29 23:30:45 -0500

    fixed alg1

--- alg1.py
+++ alg1.py
@@ -1,22 +1,26 @@
 import random
 import time
 
+# Function to perform insertion sort on an array
 def insertion_sort(arr):
-    for j in range(1, len(arr)):
-        key = arr[j]
-        i = j - 1
+    for idx in range(1, len(arr)):
+        key = arr[idx]
+        i = idx - 1
         while i >= 0 and arr[i] > key:
             arr[i + 1] = arr[i]
             i = i - 1
         arr[i + 1] = key
 
-def alg1(arr, i):
+# Custom algorithm using insertion sort
+def custom_algorithm(arr, i):
     insertion_sort(arr)
     return arr[i - 1]
 
-def generate_random_array(n):
-    return [random.randint(1, 1000) for _ in range(n)]
+# Function to generate a random array of a given size
+def generate_random_array(size):
+    return [random.randint(1, 1000) for _ in range(size)]
 
+# Function to measure the running time of an algorithm on an array
 def measure_running_time(algorithm, arr, i):
     start_time = time.time()
     result = algorithm(arr.copy(), i)
@@ -32,16 +36,18 @@ i_value = int(2 * len(input_sizes) / 3)
 # Results table header
 print("n\tTheoreticalRT\tEmpiricalRT (ms)\tRatio\tPredictedRT")
 
-for n in input_sizes:
-    theoretical_running_time = n ** 2
+# Loop through each input size
+for size in input_sizes:
+    theoretical_running_time = size ** 2
 
     # Run the algorithm multiple times and calculate average running time
     total_empirical_running_time = 0
     ratios = []
 
+    # Repeat the experiment for the specified number of iterations
     for _ in range(iterations):
-        input_array = generate_random_array(n)
-        _, empirical_running_time = measure_running_time(alg1, input_array, i_value)
+        input_array = generate_random_array(size)
+        _, empirical_running_time = measure_running_time(custom_algorithm, input_array, i_value)
         total_empirical_running_time += empirical_running_time
 
         # Calculate ratio for each iteration
@@ -54,7 +60,8 @@ for n in input_sizes:
     # Calculate predicted running time
     predicted_running_time = c1 * theoretical_running_time
 
+    # Calculate the average empirical running time
     average_empirical_running_time = total_empirical_running_time / iterations
 
-    # Format the output for better readability
-    print(f"{n}\t{theoretical_running_time:.2f}\t\t{average_empirical_running_time:.2f}\t\t{c1:.8f}\t{predicted_running_time:.2f}")
+    # Print the results in a formatted manner
+    print(f"{size}\t{theoretical_running_time:.2f}\t\t{average_empirical_running_time:.2f}\t\t{c1:.8f}\t{predicted_running_time:.2f}")
